b5ca7210228dc8b1c437b618b684b02b86397435
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-07-30 16:42:14 +0900

    0730 16:41
    적응형 스레시홀딩으로 변경, 컨투어 단순화 시도했으나 컨투어가 제대로 적용되지 않는 문제 발생

--- src/linetracer.py
+++ src/linetracer.py
@@ -13,6 +13,8 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 plt.ion()
 
 window = 'camera'
+blk_size = 9
+C = 5
 
 def masking(bp, window):
     _, mask = cv2.threshold(bp, 1, 255, cv2.THRESH_BINARY)
@@ -29,10 +31,21 @@ if cap.isOpened():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         draw = img.copy()
+        
         if ret:
-            ret2, threshold = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY_INV)
+            threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blk_size, C)
             contour, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(img, contour, -1, (255,0,0), 2)
+            mu = [None]*len(contour)
+            for i in range(len(contour)):
+                mu[i] = cv2.moments(contour[i])
+            mc = [None]*len(contour)
+            for i in range(len(contour)):
+                mc[i] = (mu[i]['m10']/(mu[i]['m00'] + 1e-5), mu[i]['m01']/(mu[i]['m00'] + 1e-5))
+            contours = contour[0]
+            epsilon = 0.05 * cv2.arcLength(contours, True)
+            approx = cv2.approxPolyDP(contours, epsilon, True)
+            cv2.circle(img, (int(mc[i][0]), int(mc[i][1])), 4, (0,0,255), -1)
+            cv2.drawContours(img, [approx], -1, (255,0,0), 2)
             cv2.imshow('camera', img)
             if cv2.waitKey(1) == ord('e'): # e 키 입력을 통해 ROI 선택 상태에 진입하도록 설정
                 (x,y,w,h) = cv2.selectROI(window, img, False)
